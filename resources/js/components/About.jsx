4e1a6c871ee7de3b40c077d66d0f7957020756d1
Chamindu Dilshan <chamindu.d@orderchimps.com>
2023-11-26 22:28:41 +0530

    add bs5

--- resources/js/components/About.jsx
+++ resources/js/components/About.jsx
@@ -3,8 +3,17 @@
 export default function About() {
     return <div className="container">
         <div className="row my-5">
-            <div className="col-md-8 mx-auto">
-                <h1> This is About</h1>
+            <div className="col-md-12 mx-auto">
+                <div class="card">
+                    <div class="card-header">
+                        About
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Hello</h5>
+                        <p class="card-text"></p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>;
--- resources/js/index.jsx
+++ resources/js/index.jsx
@@ -1,6 +1,7 @@
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const rootElement = document.getElementById('app');
 const root = createRoot(rootElement);
